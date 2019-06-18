82a09f13a8e5ca76cadad80139de37603b5e13c2
lifeifeixz <lifeifeixz@sina.cn>
2019-06-18 22:03:10 +0800

    集成pageHelper功能

--- src/main/java/org/flysli/gadget/controller/MesWorkTimeController.java
+++ src/main/java/org/flysli/gadget/controller/MesWorkTimeController.java
@@ -1,39 +1,22 @@
-package com.example.jst.controller;
+package org.flysli.gadget.controller;
 
-import com.example.jst.bean.MesWorkTimeRecord;
-import com.example.jst.bean.dto.UserAbnormalDto;
-import com.example.jst.dao.MesAbnormalTimeDao;
-import com.example.jst.service.MesWorkTimeRecordService;
-import com.github.pagehelper.Page;
 import com.github.pagehelper.PageInfo;
+import org.flysli.gadget.bean.MesWorkTimeRecord;
+import org.flysli.gadget.service.MesWorkTimeRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 public class MesWorkTimeController {
-
     @Autowired
     private MesWorkTimeRecordService workTimeRecordService;
 
-    @Autowired
-    private MesAbnormalTimeDao abnormalTimeDao;
-
-    @RequestMapping("/test")
-    public void test(){
-        workTimeRecordService.list();
-    }
 
     @GetMapping("/data")
     public PageInfo<MesWorkTimeRecord> data(){
         return workTimeRecordService.data();
     }
 
-    @RequestMapping("/relationTest")
-    public UserAbnormalDto relationTest(){
-        return abnormalTimeDao.findByUserName("JST4638");
-    }
 }
